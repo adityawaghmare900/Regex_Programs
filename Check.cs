0de072b1c04cb5a0843fe2e00da38516a332ad63
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-15 01:39:36 +0530

    [aditya]: county code follow by space and 10 digit number

--- Check.cs
+++ Check.cs
@@ -1,24 +1,11 @@
-﻿namespace LastValidName
+﻿namespace MobileFormat
 {
     internal class Check
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Select Option \n: " +
-                "1. To check for first name\n" +
-                "2. To check for last name\n");
-
-            int option=Convert.ToInt16(Console.ReadLine());
-            switch (option)
-            {
-                case 1:
-                    LastValidName.firstValid();
-                    break;
-                case 2:
-                    LastValidName.lastValid();
-                    break;
-            }
-            
+            PreDefinedMobileFormat.mobileFormat();
+        
         }
     }
 }
