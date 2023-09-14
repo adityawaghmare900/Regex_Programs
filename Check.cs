d04de95f2f22af3c7d7db909ad1b466f50345d2e
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-14 19:13:49 +0530

    [aditya]: as a user need ot enter a valid last name

--- Check.cs
+++ Check.cs
@@ -1,12 +1,24 @@
-﻿namespace ConsoleApp1
+﻿namespace LastValidName
 {
     internal class Check
     {
         static void Main(string[] args)
         {
-            ValidFirstName validFirstName = new ValidFirstName();
-            validFirstName.firstName();
-           
+            Console.WriteLine("Please Select Option \n: " +
+                "1. To check for first name\n" +
+                "2. To check for last name\n");
+
+            int option=Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    LastValidName.firstValid();
+                    break;
+                case 2:
+                    LastValidName.lastValid();
+                    break;
+            }
+            
         }
     }
 }
---
+++ LastValidName.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LastValidName
+{
+    internal class LastValidName
+    {
+        public static void firstValid()
+        {
+            Console.WriteLine("enter first name");
+            string firstname = Console.ReadLine();
+            string checkPattarn = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regex = new Regex(checkPattarn);
+            if (regex.IsMatch(firstname))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("first name is valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("first name is invalid");
+                Console.ResetColor();
+            }
+        }
+
+        public static void lastValid() 
+        {
+            Console.WriteLine("enter the last Name");
+            string lastname=Console.ReadLine();
+            string checkPattarn = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regex= new Regex(checkPattarn);
+            if(regex.IsMatch(lastname))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Last name is valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Last name is invalid");
+                Console.ResetColor();
+            }
+        }
+    }
+}
