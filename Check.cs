7790f69baca90d5f6ec9420803d41b63dc41f6c3
adityawaghmare900 <adityawaghmare900@gmail.com>
2023-09-14 18:18:28 +0530

    [aditya]: As a user need to enter a first name

---
+++ Check.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Check
+    {
+        static void Main(string[] args)
+        {
+            ValidFirstName validFirstName = new ValidFirstName();
+            validFirstName.firstName();
+           
+        }
+    }
+}
---
+++ ValidFirstName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ValidFirstName
+    {
+        public void firstName()
+        {
+            Console.Write("Enter your name: ");
+            string firstname=Console.ReadLine();
+            string namePattarn = "^[A-Z]{1}[a-zA-Z]{2,}$";
+            Regex regex=new Regex(namePattarn);
+            if (regex.IsMatch(firstname))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Name is valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Name is not valid");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+    }
+}
